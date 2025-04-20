54c67025b70eb645a76452e4ee969954be373107
ymind14563 <ymind14563@gmail.com>
2025-04-20 12:24:57 +0900

    fix: lambda 제거 및 KST 컨버터 함수화

--- menu_crawler/utils/logger.py
+++ menu_crawler/utils/logger.py
@@ -14,8 +14,12 @@ stream_handler = logging.StreamHandler()
 KST = timezone(timedelta(hours=9))
 
 # 로그 출력 형식 및 시간대 적용
+def kst_converter(*args):
+    dt = datetime.now(KST)
+    return time.localtime(time.mktime(dt.timetuple()))
+
 formatter = logging.Formatter("[%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-formatter.converter = lambda *args: time.localtime(time.mktime(datetime.now(KST).timetuple()))
+formatter.converter = kst_converter
 stream_handler.setFormatter(formatter)
 
 # 핸들러 연결
