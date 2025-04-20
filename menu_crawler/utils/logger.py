a31f4c525d5d4875412a22711e733a770c91fc75
ymind14563 <ymind14563@gmail.com>
2025-04-20 12:09:15 +0900

    fix: logger kst 추가

--- menu_crawler/utils/logger.py
+++ menu_crawler/utils/logger.py
@@ -1,12 +1,25 @@
+# 로깅 기본 설정
+
+
 import logging
+from datetime import datetime, timezone, timedelta
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+# 스트림 핸들러 생성
 stream_handler = logging.StreamHandler()
+
+# 한국 시간 (KST) 설정
+KST = timezone(timedelta(hours=9))
+
+# 로그 출력 형식 및 시간대 적용
 formatter = logging.Formatter("[%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+stream_handler.converter = lambda *args: datetime.now(KST).timetuple()
 stream_handler.setFormatter(formatter)
 
+# 핸들러 연결
 logger.addHandler(stream_handler)
 
+# 내보내기
 __all__ = ["logger"]
